4c3dc18efeb9c971d513ccbbb7683043719a26db
Noah Moroze <noahm@moroze.com>
2014-09-19 23:55:34 -0400

    Translated to jQuery

--- js/main.js
+++ js/main.js
@@ -1,32 +1,19 @@
 window.onload = function() {
-	var fb = new Firebase('https://flickering-inferno-1483.firebaseio.com/');
-
-	var submit = document.getElementById('submit');
-	var textbox = document.getElementById('message');
-	
+	var fb = new Firebase('https://flickering-inferno-1483.firebaseio.com/');	
 	var postsRef = fb.child('posts');
 
-	submit.onclick = addPost;
-
-	function addPost() {
+	$("#submit").on('click', function() {
 		postsRef.push({
-			content: textbox.value,
+			content: $("#message").val(),
 			date: new Date().toLocaleDateString('en-US')
 		});
-	}
+	});
 
 	postsRef.on('child_added', function(data) {
 		var postData = data.val();
-		var post = document.createElement('div');
-		var postContent = document.createTextNode(postData.content);
-		post.appendChild(postContent);
-		
-		var date = document.createElement('div');
-		var dateContent = document.createTextNode(postData.date);
-		date.appendChild(dateContent);
-		post.appendChild(date);
-
-		var posts = document.getElementById('messages');
-		posts.insertBefore(post, posts.firstChild);
+		var content = postData.content;
+		var date = postData.date;
+		$("#messages").prepend("<div>"+date+"</div>");		
+		$("#messages").prepend("<div>"+content+"</div>");		
 	});
 }
